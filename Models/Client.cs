7b1ef53da957b08d37b47d3e279a00fb13fb510d
Benjamin Contreras <benja.contreras732@gmail.com>
2024-02-21 23:21:55 -0300

    Agregar archivos de proyecto.

---
+++ Models/Client.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace APIClientes.Models
+{
+    public class Client
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string FirstName { get; set; }
+        [Required]
+        public string LastName { get; set; }
+        [Required]
+        public string Address { get; set; }
+        [Required]
+        public string Phone { get; set; }
+
+    }
+}
